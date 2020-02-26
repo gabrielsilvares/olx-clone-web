23c2977bb4b04b650f5b6e144ec65f01d1f80bfc
gabrielsilvares <gabrielsrcode@gmail.com>
2020-02-26 01:05:13 -0300

    config api

---
+++ src/helpers/OlxAPI.js
@@ -0,0 +1,88 @@
+import Cookies from 'js-cookie';
+import qs from 'qs';
+
+const BASEAPI = 'http://localhost:4444';
+
+const apiFetchPost = async (endpoint, body) => {
+  if (!body.token) {
+    let token = Cookies.get('token');
+    if (token) {
+      body.token = token;
+    }
+  }
+
+  const res = await fetch(BASEAPI+endpoint, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body:JSON.stringify(body)
+  });
+
+  const json = await res.json();
+
+  if (json.notallowed) {
+    window.location.href = '/signin';
+    return
+  }
+
+  return json;
+}
+
+const apiFetchGet = async (endpoint, body = []) => {
+  if (!body.token) {
+    let token = Cookies.get('token');
+    if (token) {
+      body.token = token;
+    }
+  }
+
+  const res = await fetch(`${BASEAPI+endpoint}?${qs.stringify(body)}`);
+  const json = await res.json();
+
+  if (json.notallowed) {
+    window.location.href = '/signin';
+    return
+  }
+
+  return json;
+}
+
+const OlxAPI = {
+  login: async (email, password) => {
+    const json = await apiFetchPost(
+      '/session',
+      { email, password }
+    );
+
+    return json;
+  },
+
+  register: async (name, email, password, stateLoc) => {
+    const json = await apiFetchPost(
+      '/users',
+      { name, email, password_hash: password, state: stateLoc }
+    )
+
+    return json;
+  },
+
+  getStates: async () => {
+    const json = await apiFetchGet(
+      '/states'
+    );
+    
+    return json.states;
+  },
+
+  getCategories: async () => {
+    const json = await apiFetchGet(
+      '/categories'
+    );
+    
+    return json.categories
+  }
+};
+
+export default () => OlxAPI;
