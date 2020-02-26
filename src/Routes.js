09ee020d7765b2706b891a79454fa50c3d498a7f
gabrielsilvares <gabrielsrcode@gmail.com>
2020-02-26 01:04:03 -0300

    Initial commit

---
+++ src/Routes.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp'
+
+export default () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/sobre"> 
+        <About />
+      </Route>
+      <Route exact path="/signin">
+        <SignIn />
+      </Route>
+      <Route exact path="/signup">
+        <SignUp />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch> 
+  )
+}
